d0dd8a24499b7b68338d029560aaf677f7126ec9
Home Andrey <Andrey.Kholopov.Work@gmail.com>
2023-06-14 07:21:04 +0300

    update storybook to v7.0.2

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,11 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/vue3"
-}
+  "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  "framework": {
+    name: "@storybook/vue3-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};
