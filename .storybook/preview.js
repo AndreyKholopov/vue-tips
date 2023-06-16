3ae047d0cab725f19e2b35a69c0b6e0a8494dee1
Home Andrey <Andrey.Kholopov.Work@gmail.com>
2023-06-16 12:40:30 +0300

    change autodoc

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,22 @@
+import { Title, Subtitle, Description, Primary, Controls } from '@storybook/blocks';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
+    },
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <Controls />
+        </>
+      ),
     },
   },
 }
